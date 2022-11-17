423f8daa249ab077809b708a1bd060169b921232
Preston Chang <preston.chang@stonybrook.edu>
2022-11-16 19:52:44 -0500

    HW4 as base

---
+++ client/src/components/MUIDeleteModal.js
@@ -0,0 +1,61 @@
+import { useContext } from 'react'
+import GlobalStoreContext from '../store';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    //bgcolor: 'background.paper',
+    //border: '2px solid #000',
+    //boxShadow: 24,
+    //p: 4,
+};
+
+export default function MUIDeleteModal() {
+    const { store } = useContext(GlobalStoreContext);
+    let name = "";
+    if (store.listMarkedForDeletion) {
+        name = store.listMarkedForDeletion.name;
+    }
+    function handleDeleteList(event) {
+        store.deleteMarkedList();
+    }
+    function handleCloseModal(event) {
+        store.unmarkListForDeletion();
+    }
+
+    return (
+        <Modal
+            open={store.listMarkedForDeletion !== null}
+        >
+            <Box sx={style}>
+                <div className="modal-dialog">
+                <header className="dialog-header">
+                    <Alert severity="warning" color="info">
+                        Delete the {name} playlist?
+                    </Alert>
+                </header>
+                <div id="confirm-cancel-container">
+                    <Button variant="contained"
+                        id="dialog-yes-button"
+                        className="modal-button"
+                        onClick={handleDeleteList}
+                    >Confirm</Button>
+                    <Button variant="contained"
+                        id="dialog-no-button"
+                        className="modal-button"
+                        onClick={handleCloseModal}
+                    >Cancel</Button>
+                </div>
+            </div>
+            </Box>
+        </Modal>
+    );
+}
